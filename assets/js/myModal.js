2cb6182a691e22cf709cedf7e3314f0f46ddc1a8
Joseelv_ <jose.venegasan@gmail.com>
2024-08-05 15:11:56 +0200

    update

--- assets/js/myModal.js
+++ assets/js/myModal.js
@@ -1,3 +1,4 @@
+// Abre el modal
 function openModal() {
   var modal = document.getElementById("myModal");
   var img = document.querySelector(".thumbnail");
@@ -7,7 +8,16 @@ function openModal() {
   modalImg.src = img.src;
 }
 
+// Cierra el modal
 function closeModal() {
   var modal = document.getElementById("myModal");
   modal.style.display = "none";
+}
+
+// Maneja el clic fuera del modal para cerrarlo
+window.onclick = function(event) {
+  var modal = document.getElementById("myModal");
+  if (event.target == modal) {
+      modal.style.display = "none";
+  }
 }
